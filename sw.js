d764ec0be8dc0909cace4466be9580c6a5cad777
toell-svb <series.tv.cool.lol@gmail.com>
2026-09-23 00:09:57 +0200

    Change cache name to loopie-cache-v7
    Updated cache name to version 7 for backup and reminder updates.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'loopie-cache-v6'; // Neue Version für das Backup- & Reminder-Update
+const CACHE_NAME = 'loopie-cache-v7'; // Neue Version für das Backup- & Reminder-Update
 
 const ASSETS = [
   './',
